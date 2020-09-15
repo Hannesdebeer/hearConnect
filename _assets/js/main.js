0a30c4a17d19b04e08d6399a7d95bf2afa773234
Hannes de Beer <johannesfrederikdebeer@gmail.com>
2020-09-15 22:35:48 +0200

    site update

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -39,3 +39,73 @@ var x = setInterval(function() {
     document.getElementById("timer").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+     type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ["Uniswop", "Pool 1", "Pool 2", "Pool 3", "Pool 4", "Exhange Listing Reserve"],
+        datasets: [{
+            label: "Cryptos",
+            backgroundColor: ['rgb(255, 99, 132)',
+            'rgba(255, 99, 132, 0.2)',
+               'rgba(54, 162, 235, 0.2)',
+               'rgba(255, 206, 86, 0.2)',
+               'rgba(75, 192, 192, 0.2)',
+               'rgba(153, 102, 255, 0.2)'],
+            borderColor: 'rgb(255, 99, 132,0.0)',
+            data: [15000, 5000, 5000, 5000, 7500, 2500],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+      legend: {
+        position: 'right',
+         display: true,
+         labels: {
+             fontColor: '#fff',
+         },
+
+     }
+    }
+});
+
+
+var ctx = document.getElementById('myChart2').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+     type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ["Uniswop", "Pool 1", "Pool 2", "Pool 3", "Pool 4", "Exhange Listing Reserve"],
+        datasets: [{
+            label: "Cryptos",
+            backgroundColor: ['rgb(255, 99, 132)',
+            'rgba(255, 99, 132, 0.2)',
+               'rgba(54, 162, 235, 0.2)',
+               'rgba(255, 206, 86, 0.2)',
+               'rgba(75, 192, 192, 0.2)',
+               'rgba(153, 102, 255, 0.2)'],
+            borderColor: 'rgb(255, 99, 132,0.0)',
+            data: [15000, 5000, 5000, 5000, 7500, 2500],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+      legend: {
+        position: 'right',
+         display: true,
+         labels: {
+             fontColor: '#fff',
+         },
+
+     }
+    }
+});
